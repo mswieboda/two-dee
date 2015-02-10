b8c471183874a3805218df661093addbdaa48c2d
Matt Swieboda <matt.swieboda@centro.net>
2015-02-10 13:22:45 -0600

    initial Gosu app

---
+++ game.rb
@@ -0,0 +1,17 @@
+require 'gosu'
+
+class MyWindow < Gosu::Window
+  def initialize
+    super(640, 480, false)
+    self.caption = 'Hello World!'
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
+
+window = MyWindow.new
+window.show
